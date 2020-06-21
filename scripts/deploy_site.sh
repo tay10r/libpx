91f7440d3f0cc3140d1340dd1b77698f01ae7f25
Taylor Holberton <taylorcholbrcholberton@gmail.com>
2020-06-21 08:11:43 -0500

    Fixed web deploy script

--- scripts/deploy_site.sh
+++ scripts/deploy_site.sh
@@ -13,13 +13,13 @@ site_dir="$working_dir/libpx_site"
 
 time_stamp=`date +%F--%H:%M:%S`
 
-release_stamp=$time_stamp--$commit
+release_stamp="$time_stamp--$commit"
 
 editor_src_dir="$PWD/emscripten-build/editor"
-editor_dst_dir="$site_dir/editor/$release_stamp"
+editor_dst_dir="$site_dir/releases/$release_stamp/editor"
 
 doc_src_dir="$PWD/doxygen"
-doc_dst_dir="$site_dir/doxygen/$release_stamp"
+doc_dst_dir="$site_dir/releases/$release_stamp/doxygen"
 
 if [ ! -e "$editor_src_dir/pxedit_sdl.html" ]; then
   echo "Cannot find webpage."
@@ -43,9 +43,11 @@ cp "$editor_src_dir/pxedit_sdl.wasm" "$editor_dst_dir"
 
 cp -R "$doc_src_dir/html/"* "$doc_dst_dir"
 
-ln -s "editor/$release_stamp" "editor/latest"
+rm -f "editor/latest"
+rm -f "doxygen/latest"
 
-ln -s "doxygen/$release_stamp" "doxygen/latest"
+ln -s "releases/$release_stamp/editor/" "editor"
+ln -s "releases/$release_stamp/doxygen/" "doxygen"
 
 cat >index.md << EOF
 libpx
@@ -57,9 +59,9 @@ Last updated $(date).
 
 Using commit [$commit](https://github.com/tay10r/libpx/tree/$commit).
 
-Visit [here](doxygen/latest/index.html) for the latest documentation.
+Visit [here](doxygen/index.html) for the latest documentation.
 
-Visit [here](editor/latest/pxedit_sdl.html) to use the latest editor release.
+Visit [here](editor/pxedit_sdl.html) to use the latest editor release.
 EOF
 
 git add .
