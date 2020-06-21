cb95fd5f13114ea88a5fed30b23bf55d611b1319
Taylor Holberton <taylorcholbrcholberton@gmail.com>
2020-06-21 07:16:05 -0500

    Fixed symbolic links

--- scripts/deploy_site.sh
+++ scripts/deploy_site.sh
@@ -11,13 +11,15 @@ working_dir="/tmp"
 
 site_dir="$working_dir/libpx_site"
 
-tstamp=`date +%F--%H:%M:%S`
+time_stamp=`date +%F--%H:%M:%S`
+
+release_stamp=$time_stamp--$commit
 
 editor_src_dir="$PWD/emscripten-build/editor"
-editor_dst_dir="$site_dir/editor/$tstamp--$commit"
+editor_dst_dir="$site_dir/editor/$release_stamp"
 
 doc_src_dir="$PWD/doxygen"
-doc_dst_dir="$site_dir/doxygen/$tstamp--$commit"
+doc_dst_dir="$site_dir/doxygen/$release_stamp"
 
 if [ ! -e "$editor_src_dir/pxedit_sdl.html" ]; then
   echo "Cannot find webpage."
@@ -41,9 +43,9 @@ cp "$editor_src_dir/pxedit_sdl.wasm" "$editor_dst_dir"
 
 cp -R "$doc_src_dir/html/"* "$doc_dst_dir"
 
-ln -s "$editor_dst_dir" "$site_dir/editor/latest"
+ln -s "editor/$release_stamp" "editor/latest"
 
-ln -s "$doc_dst_dir" "$site_dir/doxygen/latest"
+ln -s "doxygen/$release_stamp" "doxygen/latest"
 
 cat >index.md << EOF
 libpx
