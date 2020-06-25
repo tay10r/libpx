9b033821204e471080915920d85da57fa3be581a
Taylor Holberton <taylorcholbrcholberton@gmail.com>
2020-06-24 20:42:37 -0500

    Deploy script will now overwrite last version

--- scripts/deploy_site.sh
+++ scripts/deploy_site.sh
@@ -15,13 +15,13 @@ time_stamp=`date +%F--%H:%M:%S`
 
 release_stamp="$time_stamp--$commit"
 
-editor_src_dir="$PWD/emscripten-build/editor"
+editor_src_dir="$PWD/browser-build/editor"
 editor_dst_dir="$site_dir/releases/$release_stamp/editor"
 
 doc_src_dir="$PWD/doxygen"
 doc_dst_dir="$site_dir/releases/$release_stamp/doxygen"
 
-if [ ! -e "$editor_src_dir/pxedit_sdl.html" ]; then
+if [ ! -e "$editor_src_dir/pxedit_browser.html" ]; then
   echo "Cannot find webpage."
   exit 1
 fi
@@ -33,18 +33,15 @@ fi
 
 doxygen
 
-git clone git@github.com:tay10r/libpx_site.git "$site_dir" && cd "$site_dir"
+mkdir "$site_dir" && cd "$site_dir" && git init
 
 mkdir -p "$doc_dst_dir" "$editor_dst_dir"
 
-cp "$editor_src_dir/pxedit_sdl.html" "$editor_dst_dir"
-cp "$editor_src_dir/pxedit_sdl.js"   "$editor_dst_dir"
-cp "$editor_src_dir/pxedit_sdl.wasm" "$editor_dst_dir"
+cp "$editor_src_dir/pxedit_browser.html" "$editor_dst_dir"
+cp "$editor_src_dir/pxedit_browser.js"   "$editor_dst_dir"
+cp "$editor_src_dir/pxedit_browser.wasm" "$editor_dst_dir"
 
 cp -R "$doc_src_dir/html/"* "$doc_dst_dir"
-
-rm -f "editor"
-rm -f "doxygen"
 
 ln -s "releases/$release_stamp/editor/" "editor"
 ln -s "releases/$release_stamp/doxygen/" "doxygen"
@@ -61,7 +58,7 @@ Using commit [$commit](https://github.com/tay10r/libpx/tree/$commit).
 
 Visit [here](doxygen/index.html) for the latest documentation.
 
-Visit [here](editor/pxedit_sdl.html) to use the latest editor release.
+Visit [here](editor/pxedit_browser.html) to use the latest editor release.
 EOF
 
 git add .
@@ -72,7 +69,7 @@ New release from deployment script.
 Commit: $commit
 EOF
 
-git push origin master
+git push --force git@github.com:tay10r/libpx_site.git master
 
 cd "$original_dir"
 
